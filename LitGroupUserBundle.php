d375f6ab60771abef5b4af5096b55611e154fa7e
Roman Shamritskiy <roman@litgroup.ru>
2014-01-06 21:08:34 +0400

    Added empty bundle class.

---
+++ LitGroupUserBundle.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * This file is part of the "LitGroupUserBundle" package.
+ *
+ * (c) Roman Shamritskiy <roman@litgroup.ru>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace LitGroup\Bundle\UserBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * LitGroupUserBundle
+ *
+ * @author Roman Shamritskiy <roman@litgroup.ru>
+ */
+class LitGroupUserBundle extends Bundle
+{
+
+}
