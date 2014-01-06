aaf8f7778923ebc3d47b2bb1a369fcf6ca831843
Roman Shamritskiy <roman@litgroup.ru>
2014-01-06 21:57:03 +0400

    Added basic UserInterface and NamedUserInterface for username support. [ci skip]

---
+++ Model/User/NamedUserInterface.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * This file is part of the "LitGroupUserBundle" package.
+ *
+ * (c) Roman Shamritskiy <roman@litgroup.ru>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace LitGroup\Bundle\UserBundle\Model\User;
+
+/**
+ * NamedUserInterface
+ *
+ * Interface provides username support for user.
+ *
+ * @author Roman Shamritskiy <roman@litgroup.ru>
+ */
+interface NamedUserInterface extends UserInterface
+{
+    /**
+     * Sets username.
+     *
+     * @param string $username
+     *
+     * @return self
+     */
+    public function setUsername($username);
+} 
---
+++ Model/User/UserInterface.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * This file is part of the "LitGroupUserBundle" package.
+ *
+ * (c) Roman Shamritskiy <roman@litgroup.ru>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace LitGroup\Bundle\UserBundle\Model\User;
+
+use Symfony\Component\Security\Core\User\UserInterface as SecurityUserInterface;
+
+/**
+ * UserInterface
+ *
+ * @author Roman Shamritskiy <roman@litgroup.ru>
+ */
+interface UserInterface extends SecurityUserInterface
+{
+    /**
+     * Sets user's email.
+     *
+     * @param string $email
+     *
+     * @return self
+     */
+    public function setEmail($email);
+
+    /**
+     * Returns user's email.
+     *
+     * @return string
+     */
+    public function getEmail();
+
+    /**
+     * Sets encoded password.
+     *
+     * @param string $password
+     *
+     * @return self
+     */
+    public function setPassword($password);
+
+    /**
+     * Sets plain (non-encoded) password.
+     *
+     * @param string $plainPassword
+     *
+     * @return self
+     */
+    public function setPlainPassword($plainPassword);
+
+    /**
+     * Returns plain (non-encoded) password.
+     *
+     * @return string
+     */
+    public function getPlainPassword();
+}
